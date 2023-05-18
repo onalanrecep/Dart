3194b4587f9158cede0cac33ea51f209a88c6a12
onalanrecep <onalanrecep86@gmail.com>
2023-05-18 23:26:19 +0300

    this ve late kavramları null safety

--- hafta4/class.dart
+++ hafta4/class.dart
@@ -8,11 +8,12 @@ void main() {
 
   // bankaya 3 müşteri geldi, birinin 100 tl'si var, birinin hesabı yok diğerinin 0₺ si var.
   // Hesabı olmayana hesap açalım. 0₺ olanı kov, 100₺ olana müşterim hoş geldin de
-     int? controlMoney(int? money) {
+  int? controlMoney(int? money) {
     if (money != null && money > 0) {
       return money;
     }
   }
+
   List<int?> customerMoneys = [100, null, 0];
   for (var item in customerMoneys) {
     if (item != null) {
@@ -24,9 +25,46 @@ void main() {
     } else {
       print('haydi hesap açalım');
     }
- 
 
     bool result = controlMoney(item) == null ? false : true;
     print(result);
+  }
+  print('___________________________');
+  User user1 = User('Reko', 1200, age: 37, city: 'Bingöl');
+  User user2 = User('Hiko', 120);
+  final user3 = User('Rufi', 256);
+  print(user3.name);
+  print(user1.userCode);
+  print('____________' * 10);
+  //Müşteri son gelen kişinin City'sine göre kampanya yapacak eğer İstanbul ise
+  if (user3.city == null) {
+    print('Müşteri Şehir Bilgisi vermemiş');
+  } else {
+    if (user3.city == 'istanbul') {
+      print('Tebrikler Kazandınız');
+    } else {
+      print('Tekrar deneyiniz');
+    }
+  }
+}
+
+class User {
+  // adı olmak zorunda
+  // parası olmak zorunda
+  // yaşını vermek zorunda değil
+  // city vermek zorunda değil
+  late final String name;
+  late final int money;
+  late final int? age;
+  late final String? city;
+
+  late final String userCode;
+
+  User(String name, int money, {int? age, String? city}) {
+    this.name = name;
+    this.money = money;
+    this.age = age;
+    this.city = city;
+    userCode = (city ?? 'istanbul') + name;
   }
 }
